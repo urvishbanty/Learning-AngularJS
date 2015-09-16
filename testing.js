a123639846a0126e6278610d5b5d78937e5863ee
James Beamer <jmb2331@vt.edu>
2015-09-15 23:06:49 -0400

    Update testing.js
    I pretty much scrapped the previous edition in order to now alert the user what he/she input his/herself.

--- testing.js
+++ testing.js
@@ -1,8 +1,13 @@
 /**
-* Name: myFunction
+* Name: javaScriptAlert
 * Purpose: To alert user when the function is called
 * by having a pop-up.
 */
-function myFunction() {
-    alert("Confirmation!!");
+
+function javaScriptAlert()
+{
+	var inputField = document.myform.Field.value;
+             
+    alert( document.getElementById('Alerting').value = inputField);
 }
+        
