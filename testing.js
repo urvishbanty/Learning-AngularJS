cedd2abe2f7a7ea59e2d9ec930aaa6aa0dcd569c
jmb2331 <jmb2331@vt.edu>
2015-10-03 15:41:32 -0400

    Learning how to use SourceTree. This basically patches up a mistake I made while learning.

---
+++ testing.js
@@ -0,0 +1,13 @@
+/**
+* Name: javaScriptAlert
+* Purpose: To alert user when the function is called
+* by having a pop-up.
+*/
+
+function javaScriptAlert()
+{
+	var inputField = document.myform.Field.value;
+             
+    alert( document.getElementById('Alerting').value = inputField);
+}
+        
